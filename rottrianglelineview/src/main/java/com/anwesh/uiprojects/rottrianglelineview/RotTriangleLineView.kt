e6d685fa6b3444f5b0203f1ae8f8daaf99c7514d
Anwesh43 <anweshthecool0@gmail.com>
2019-09-29 11:58:11 +0530

    adding Canvas extension methods

--- rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
+++ rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
@@ -20,9 +20,48 @@ val strokeFactor : Float = 90f
 val foreColor : Int = Color.parseColor("#E65100")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 40
+val sizeFactor : Float = 2.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
-fun Float.x(deg : Float, r : Float) : Float = r * Math.cos(deg * Math.PI / 180).toFloat()
-fun Float.y(deg : Float, r : Float) : Float = r * Math.sin(deg * Math.PI / 180).toFloat()
+fun Float.x(deg : Float) : Float = this * Math.cos(deg * Math.PI / 180).toFloat()
+fun Float.y(deg : Float) : Float = this * Math.sin(deg * Math.PI / 180).toFloat()
+
+
+fun Canvas.drawTriPath(size : Float, deg : Float, paint : Paint) {
+    val x : Float = size.x(deg)
+    val y : Float = size.y(deg)
+    val path : Path = Path()
+    path.moveTo(0f, 0f)
+    path.lineTo(x, 0f)
+    path.lineTo(x, -y)
+    path.lineTo(0f, 0f)
+    drawPath(path, paint)
+}
+
+fun Canvas.drawRotTriLine(i : Int, size : Float, sc : Float, paint : Paint) {
+    val deg : Float = 45f * sc.divideScale(i, lines)
+    save()
+    rotate(deg)
+    drawLine(0f, 0f, size, 0f, paint)
+    restore()
+    drawTriPath(size / 2, deg, paint)
+}
+
+fun Canvas.drawRotTriLines(size : Float, sc : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawRotTriLine(j, size, sc, paint)
+    }
+}
+
+fun Canvas.drawRTLNode(i : Int, sc : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawRotTriLines(size, sc, paint)
+    restore()
+}

921308f56b022a1a15300a5d86f50ee772dd84ed
Anwesh43 <anweshthecool0@gmail.com>
2019-09-29 11:59:36 +0530

    adding RotTriangleLineView class

--- rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
+++ rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
@@ -65,3 +65,21 @@ fun Canvas.drawRTLNode(i : Int, sc : Float, paint : Paint) {
     drawRotTriLines(size, sc, paint)
     restore()
 }
+
+class RotTriangleLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

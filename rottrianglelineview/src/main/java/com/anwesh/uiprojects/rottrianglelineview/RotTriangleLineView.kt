3d47d65fc2a6269c8b3f2959284ba913bbd917ee
Anwesh43 <anweshthecool0@gmail.com>
2019-09-29 12:12:29 +0530

    adding RotTriangleLine class

--- rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
+++ rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
@@ -169,7 +169,31 @@ class RotTriangleLineView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class RotTriangleLine(var i : Int) {
+
+        private val root : RTLNode = RTLNode(0)
+        private var curr : RTLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }

30fa05562c96e89f851b2667adf3159b954cc2a6
Anwesh43 <anweshthecool0@gmail.com>
2019-09-29 11:46:29 +0530

    adding Constants

--- rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
+++ rottrianglelineview/src/main/java/com/anwesh/uiprojects/rottrianglelineview/RotTriangleLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.rottrianglelineview
 /**
  * Created by anweshmishra on 29/09/19.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.01f
+val strokeFactor : Float = 90f
+val foreColor : Int = Color.parseColor("#E65100")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 40
